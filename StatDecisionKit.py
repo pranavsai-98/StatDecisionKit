92687cd7f4ca2220fa00c22af094de5176e56cb3
Ganesh <ganeshghimire40@gmail.com>
2023-12-03 12:05:43 -0500

    analyze_feature method added
    added analyze_feature method withint he class.

--- StatDecisionKit.py
+++ StatDecisionKit.py
@@ -26,3 +26,38 @@ class StatisticalTestRunner:
             return 'numerical'
         else:
             return 'categorical'
+        
+    
+    # __________________________________________________________________________________________Manoj
+    # __________________________________________________________________________________________Ganesh
+    @staticmethod
+    def analyze_features(data, target_variable, alpha=0.05):
+        """
+        Analyze features in a DataFrame against a target variable. Drops features 
+        which are not significant and prints out significant and non-significant features.
+
+        :param data: Pandas DataFrame containing the features and target variable.
+        :param target_variable: The name of the target variable.
+        :param alpha: Significance level, defaults to 0.05.
+        :return: DataFrame with only significant features.
+        """
+        significant_features = []
+        non_significant_features = []
+
+        for feature in data.columns:
+            if feature != target_variable:
+                test_name = StatisticalTestRunner.determine_statistical_test(
+                    data, feature, target_variable)
+                result = StatisticalTestRunner.execute_statistical_test(
+                    data, feature, target_variable)
+
+                if result.get('p_value', 1) < alpha:
+                    significant_features.append(feature)
+                else:
+                    non_significant_features.append(feature)
+
+        # Printing the results
+        print("Significant features:", significant_features)
+        print("Non-significant features:", non_significant_features)
+
+        return data[significant_features + [target_variable]]
