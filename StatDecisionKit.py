c8679a259a1043a0a4336fc6fa854eb78fbb4e91
MANOJKUMAR1302 <112432831+MANOJKUMAR1302@users.noreply.github.com>
2023-12-03 22:50:51 -0500

    Update StatDecisionKit.py

--- StatDecisionKit.py
+++ StatDecisionKit.py
@@ -26,10 +26,51 @@ class StatisticalTestRunner:
             return 'numerical'
         else:
             return 'categorical'
-        
     
-    # __________________________________________________________________________________________Manoj
-    # __________________________________________________________________________________________Ganesh
+    @staticmethod
+    def determine_statistical_test(data, feature1, feature2=None, paired=False):
+        """
+        Determine the appropriate statistical test based on the data and the names of one or two features.
+
+        :param data: The dataset (pandas DataFrame).
+        :param feature1: The name of the first feature (column).
+        :param feature2: The name of the second feature (column), optional.
+        :param paired: Boolean indicating if the samples are paired, applicable for two numerical features.
+        :return: Name of the statistical test.
+        """
+        feature1_type = StatisticalTestRunner.get_feature_type(data, feature1)
+        feature2_type = StatisticalTestRunner.get_feature_type(
+            data, feature2) if feature2 else None
+        sample_size = len(data)
+
+        if feature2_type is None:
+            # Single feature analysis
+            if feature1_type == 'categorical':
+                return "Chi-squared goodness of fit test"
+            elif feature1_type == 'numerical':
+                return "One-sample t-test" if sample_size <= 30 else "Z-test"
+
+        elif feature1_type == feature2_type:
+            # Both features are of the same type
+            if feature1_type == 'categorical':
+                return "Chi-squared test of independence"
+            elif feature1_type == 'numerical':
+                if paired:
+                    return "Paired t-test"
+                elif sample_size > 30:
+                    return "Two-sample t-test"
+                else:
+                    return "ANOVA"
+
+        else:
+            # Features are of different types
+            if feature1_type == 'numerical' and feature2_type == 'categorical':
+                return "ANOVA"
+            elif feature1_type == 'categorical' and feature2_type == 'numerical':
+                return "ANOVA"
+
+        return "Unable to determine an appropriate test"
+        
     @staticmethod
     def analyze_features(data, target_variable, alpha=0.05):
         """
